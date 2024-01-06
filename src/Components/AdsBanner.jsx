8794ded7a361d996d680b538875b07055c2a8225
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-01-06 19:50:52 +0500

    ads banner

--- src/Components/AdsBanner.jsx
+++ src/Components/AdsBanner.jsx
@@ -4,11 +4,23 @@ import "../Styles/adsbanner.scss";
 // Images
 import adsBannerImg from "../Images/ads-banner.svg";
 
+// Components
+import Cta from "./Cta";
+
 function AdsBanner() {
   return (
-    <div>
+    <div className="ads-section">
       <div className="adsbanner">
         <img src={adsBannerImg} alt="ads banner" />
+        <div className="ads-card">
+          <h1>Kuchli mutaxasis kerakmi?</h1>
+          <p>
+            Bizning uzoq yillik tajribaga ega mutaxassizlardan tarkib topgan
+            jamoamiz sizning gaz plitangizga doir har qanday muammoga yechim
+            berishga tayyor.
+          </p>
+          <Cta txt={"Qo'ng'iroq qilish"} />
+        </div>
       </div>
     </div>
   );
